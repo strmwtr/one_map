cba302ee0ff2405ae81c8b4cabf3f48d02ceb04b
Bob Brown <rb3@tutanota.com>
2019-06-25 18:10:58 -0400

    Added comments for documentation

--- onemap.py
+++ onemap.py
@@ -1,47 +1,24 @@
+'''
+Author: Bob Brown - brownr@charlottesville.org
+Created: Summer 2019
+Purpose: Create database and data structure for Bicycle and Pedestrian One Map project.
+Repo: https://github.com/strmwtr/one_map
+'''
+
 import arcpy
 
 def one_map_gdb(out_dir, gdb_name):
+    '''Creates geodatabase to house data. out_dir is an existing directory
+       gdb_name is the name of the gdb'''
     arcpy.CreateFileGDB_management(out_dir, gdb_name)
 
 def one_map_domains(gdb_path):
+    '''Creates the domains for the fields. Domains were outlined by One Map 
+       steering committee '''
     arcpy.CreateDomain_management(gdb_path, domName, "Valid pipe materials", "TEXT", "CODED")
 
-def one_map_feature(out_dir, out_name): 
-
-  fields = [
-    ['BufferType','Text',12],
-    ['BufferWidthFeet','Short','NA'],
-    ['CommunityAdvisoryCommittee','Text',100],
-    ['Condition','Text',20],
-    ['ConditionDate','Date','NA'],
-    ['Curbs','Text',3],
-    ['DataSource','Text',5],
-    ['Easement','Text',3],
-    ['LastEditor','Text',50],
-    ['LastEditedDate','Date','NA'],
-    ['MagisterialDistrict','Text',20],
-    ['Maintenance','Text',20],
-    ['Name','Text',100],
-    ['Notes','Text',500],
-    ['OwnerName','Text',100],
-    ['OwnershipType','Text',25],
-    ['Planning Status','Text',25],
-    ['PriorityRank2018','Short','NA'],
-    ['PriorityRank2019','Short','NA'],
-    ['RoadAADT','Short','NA'],
-    ['RoadSpeedLimit','Short','NA'],
-    ['RouteName','Text',100],
-    ['RouteNumber','Text',8],
-    ['Signage','Text',3],
-    ['SourceDocument','Text',100],
-    ['Status','Text',9],
-    ['SurfaceMaterial','Text',20],
-    ['SurfaceCondition','Text',10],
-    ['FacilityType','Text',25],
-    ['WidthFeet','Short','NA'],
-    ['YearBuilt','Short','NA']
-    ]
-
+def one_map_feature(out_dir, out_name, fields): 
+  '''Creates One Map polyline feature class'''
   arcpy.CreateFeatureclass_management(
     out_path = out_location, 
     out_name = out_name,
@@ -49,20 +26,21 @@ def one_map_feature(out_dir, out_name):
     spatial_reference = "PROJCS['NAD_1983_StatePlane_Virginia_South_FIPS_4502_Feet',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Lambert_Conformal_Conic'],PARAMETER['False_Easting',11482916.66666666],PARAMETER['False_Northing',3280833.333333333],PARAMETER['Central_Meridian',-78.5],PARAMETER['Standard_Parallel_1',36.76666666666667],PARAMETER['Standard_Parallel_2',37.96666666666667],PARAMETER['Latitude_Of_Origin',36.33333333333334],UNIT['Foot_US',0.3048006096012192]];-111140600 -91532100 3048.00609601219;-100000 10000;-100000 10000;3.28083333333333E-03;0.001;0.001;IsHighPrecision", 
     )
 
-  for x in fields:
-    if x[2] != 'NA':
+  for field in fields:
+    if field[2] != 'NA':
       arcpy.Addfield_management(
-        in_table = r'{0}\{1}'.format(out_location, out_name), 
-        field_name = x[0], 
-        field_type = x[1],
-        field_length = x[2])
+        in_table = out_ft, 
+        field_name = field[0], 
+        field_type = field[1],
+        field_length = field[2])
     else:
       arcpy.AddField_management(
-        in_table = r'{0}\{1}'.format(out_location, out_name), 
-        field_name = x[0], 
-        field_type = x[1])
+        in_table = out_ft, 
+        field_name = field[0], 
+        field_type = field[1])
 
 def assign_domains():
+  '''Assigns domains to OneMap features'''
   arcpy.AssignDomainToField_management(out_ft, field_name, domain_name)
 
 calls():
@@ -73,8 +51,40 @@ calls():
     out_name = 'OneMap'
     out_ft = f'{gdb_path}/{out_name}'
     gdb_path = f'{out_dir}/{gdb_name}'
+
+    fields = [
+        ['BufferType','Text',12],
+        ['BufferWidthFeet','Short','NA'],
+        ['CommunityAdvisoryCommittee','Text',100],
+        ['Condition','Text',20],
+        ['ConditionDate','Date','NA'],
+        ['Curbs','Text',3],
+        ['DataSource','Text',5],
+        ['Easement','Text',3],
+        ['LastEditor','Text',50],
+        ['LastEditedDate','Date','NA'],
+        ['MagisterialDistrict','Text',20],
+        ['Maintenance','Text',20],
+        ['Name','Text',100],
+        ['Notes','Text',500],
+        ['OwnerName','Text',100],
+        ['OwnershipType','Text',25],
+        ['Planning Status','Text',25],
+        ['PriorityRank2018','Short','NA'],
+        ['PriorityRank2019','Short','NA'],
+        ['RoadAADT','Short','NA'],
+        ['RoadSpeedLimit','Short','NA'],
+        ['RouteName','Text',100],
+        ['RouteNumber','Text',8],
+        ['Signage','Text',3],
+        ['SourceDocument','Text',100],
+        ['Status','Text',9],
+        ['SurfaceMaterial','Text',20],
+        ['SurfaceCondition','Text',10],
+        ['FacilityType','Text',25],
+        ['WidthFeet','Short','NA'],
+        ['YearBuilt','Short','NA']
+        ]
+
     one_map_gdb(out_location,out_name)
     one_map_feature(out_location, out_name)
-
-#arcpy.CreateDomain_management("montgomery.gdb", domName, "Valid pipe materials", "TEXT", "CODED")
-#arcpy.AssignDomainToField_management(in_table, field_name, domain_name, {subtype_code})

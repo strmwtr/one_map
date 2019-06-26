c71a531d8554cad1518a5c5964470d7c21395d8c
Bob Brown <rb3@tutanota.com>
2019-06-26 08:40:38 -0400

    Refactored code to be more reusable

--- onemap.py
+++ onemap.py
@@ -20,7 +20,7 @@ def one_map_domains(gdb_path, domain_list):
 def one_map_feature(out_dir, out_name, fields): 
   '''Creates One Map polyline feature class'''
   arcpy.CreateFeatureclass_management(
-    out_path = out_location, 
+    out_path = gdb_path, 
     out_name = out_name,
     geometry_type = 'POLYLINE', 
     spatial_reference = "PROJCS['NAD_1983_StatePlane_Virginia_South_FIPS_4502_Feet',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Lambert_Conformal_Conic'],PARAMETER['False_Easting',11482916.66666666],PARAMETER['False_Northing',3280833.333333333],PARAMETER['Central_Meridian',-78.5],PARAMETER['Standard_Parallel_1',36.76666666666667],PARAMETER['Standard_Parallel_2',37.96666666666667],PARAMETER['Latitude_Of_Origin',36.33333333333334],UNIT['Foot_US',0.3048006096012192]];-111140600 -91532100 3048.00609601219;-100000 10000;-100000 10000;3.28083333333333E-03;0.001;0.001;IsHighPrecision", 
@@ -46,9 +46,7 @@ def assign_domains(fields):
     arcpy.AssignDomainToField_management(gdb_path, field[0], field[3])
 
 
-calls():
-    out_location = r'C:\Users\brownr\Desktop\db\PoncyA\OneMap\OneMap.gdb'
-    out_dir =   r'C:\Users\brownr\Desktop\db\PoncyA\OneMap'
+calls(out_dir):
     gdb_name = r'OneMap.gdb'
     out_name = 'OneMap'
     out_ft = f'{gdb_path}/{out_name}'
@@ -100,7 +98,10 @@ calls():
         ]
         
     arcpy.Delete_management(gdb_path)
-    one_map_gdb(out_location,out_name)
+    one_map_gdb(gdb_path,out_name)
     one_map_domains(gdb_path, domain_list)
-    one_map_feature(out_location, out_name)
+    one_map_feature(gdb_path, out_name)
     assign_domains(fields)
+
+
+calls(r'C:\Users\brownr\Desktop\db\PoncyA\OneMap')
